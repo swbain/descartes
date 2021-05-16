c5bed6b12d396dfeb6205e9c7873bc48d2eced1c
Stephen Bain <swbain@gmail.com>
2021-05-16 00:55:11 -0500

    configure step length with encoders

--- descartes.lua
+++ descartes.lua
@@ -13,19 +13,33 @@ local y_step = 1
 local active_x = 1
 local active_y = 1
 
-local x_div = 1
-local y_div = 4
-
-function init() 
+function init()
+    init_params()
     redraw()
     grid_redraw()
-
     clock.run(tick)
 end
 
+function init_params()
+    params:add_number("x_div", "x div", 1, 32, 1)
+    params:add_number("y_div", "y div", 1, 32, 4)
+end
+
+function enc(n, d)
+    if n == 2 then
+        params:delta("x_div", d)
+    elseif n == 3 then
+        params:delta("y_div", d)
+    end
+    redraw()
+end
+
 function redraw()
+    screen.clear()
     screen.move(64, 32)
-    screen.text_center("hello world")
+    screen.text_center("x div = " .. params:get("x_div"))
+    screen.move(64, 44)
+    screen.text_center("y div = "  .. params:get("y_div"))
     screen.update()
 end
 
@@ -52,14 +66,23 @@ function tick()
 end
 
 function tick_x()
-    if x_div == 1 then
+    if params:get("x_div") == 1 then
         active_x = active_x % 4 + 1
+    else
+        x_step = x_step % params:get("x_div") + 1
+        if x_step % params:get("x_div") == 1 then
+            active_x = active_x % 4 + 1
+        end
     end
 end
 
 function tick_y()
-    y_step = y_step % y_div + 1
-    if y_step % y_div == 1 then
+    if params:get("y_div") == 1 then
         active_y = active_y % 4 + 1
+    else
+        y_step = y_step % params:get("y_div") + 1
+        if y_step % params:get("y_div") == 1 then
+            active_y = active_y % 4 + 1
+        end
     end
 end
