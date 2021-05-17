a66350b3d8bc07eab56fa0dabfe36579351729e0
Stephen Bain <swbain@gmail.com>
2021-05-17 14:59:58 -0500

    abstracted grid sections, hardcoded divs for now

--- descartes.lua
+++ descartes.lua
@@ -4,82 +4,56 @@ g = grid.connect()
 local width = 4
 local height = 4
 
-local x_step = 1
-local y_step = 1
+local Section = include("lib/section")
 
-local active_x = 1
-local active_y = 1
+local sections = {
+    Section:new(),
+    Section:new { x_offset = 6, x_div = 4, y_div = 1 },
+    Section:new { x_offset = 12, x_div = 1, y_div = 1 },
+}
+
 
 function init()
-    init_params()
     redraw()
     grid_redraw()
     clock.run(tick)
 end
 
-function init_params()
-    params:add_number("x_div", "x div", 1, 32, 1)
-    params:add_number("y_div", "y div", 1, 32, 4)
-end
-
-function enc(n, d)
-    if n == 2 then
-        params:delta("x_div", d)
-    elseif n == 3 then
-        params:delta("y_div", d)
-    end
-    redraw()
-end
-
 function redraw()
     screen.clear()
     screen.move(64, 32)
-    screen.text_center("x div = " .. params:get("x_div"))
+    screen.text_center("x div = " .. sections[1].x_div)
     screen.move(64, 44)
-    screen.text_center("y div = "  .. params:get("y_div"))
+    screen.text_center("y div = "  .. sections[1].y_div)
     screen.update()
 end
 
 function grid_redraw() 
+    for i = 1, #sections do
+        draw_section_grid(sections[i])
+    end
+    
+    g:refresh()
+end
+
+function draw_section_grid(section)
     for y = 1, height do
-        for x = 1, width do
-            if x == active_x and y == active_y then
+        for x = 1 + section.x_offset, width + section.x_offset do
+            if x == section.active_x + section.x_offset and y == section.active_y then
                 g:led(x, y, 13)
             else
                 g:led(x, y, 3)
             end
         end
     end
-    g:refresh()
 end
 
 function tick()
     while true do
         clock.sync(1)
-        tick_x()
-        tick_y()
+        for i = 1, #sections do
+            sections[i]:tick()
+        end
         grid_redraw()
-    end
-end
-
-function tick_x()
-    if params:get("x_div") == 1 then
-        active_x = active_x % 4 + 1
-    else
-        x_step = x_step % params:get("x_div") + 1
-        if x_step % params:get("x_div") == 1 then
-            active_x = active_x % 4 + 1
-        end
-    end
-end
-
-function tick_y()
-    if params:get("y_div") == 1 then
-        active_y = active_y % 4 + 1
-    else
-        y_step = y_step % params:get("y_div") + 1
-        if y_step % params:get("y_div") == 1 then
-            active_y = active_y % 4 + 1
-        end
     end
 end
---
+++ lib/section.lua
@@ -0,0 +1,36 @@
+local Section = { x_div = 1, y_div = 4, active_x = 1, active_y = 1, x_step = 1, y_step = 1, x_offset = 0 }
+
+function Section:new(o)
+    o = setmetatable(o or {}, Section)
+    self.__index = self
+    return o
+end
+
+function Section:tick()
+    self:tick_x()
+    self:tick_y()
+end
+
+function Section:tick_x()
+    if self.x_div == 1 then
+        self.active_x = self.active_x % 4 + 1
+    else
+        self.x_step = self.x_step % self.x_div + 1
+        if self.x_step % self.x_div == 1 then
+            self.active_x = self.active_x % 4 + 1
+        end
+    end
+end
+
+function Section:tick_y()
+    if self.y_div == 1 then
+        self.active_y = self.active_y % 4 + 1
+    else
+        self.y_step = self.y_step % self.y_div + 1
+        if self.y_step % self.y_div == 1 then
+            self.active_y = self.active_y % 4 + 1
+        end
+    end
+end
+
+return Section
